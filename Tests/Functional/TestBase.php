23195455950787a13b140738a8785f825f98f538
Vagrant User <vagrant@ongr.dev>
2015-02-17 14:24:52 +0000

    refactoring test base class

--- Tests/Functional/TestBase.php
+++ Tests/Functional/TestBase.php
@@ -16,6 +16,7 @@ use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\DriverManager;
 use Doctrine\ORM\EntityManager;
 use Symfony\Bridge\Doctrine\RegistryInterface;
+use Symfony\Bundle\FrameworkBundle\Client;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
@@ -28,6 +29,55 @@ abstract class TestBase extends WebTestCase
      * @var ContainerInterface
      */
     private $container;
+
+    /**
+     * Sets up required info before each test.
+     */
+    protected function setUp()
+    {
+        AnnotationRegistry::registerFile(
+            'vendor/doctrine/orm/lib/Doctrine/ORM/Mapping/Driver/DoctrineAnnotations.php'
+        );
+
+        $container = static::createClient()->getContainer();
+
+        $name = $container->getParameter('database_name');
+
+        $connection = DriverManager::getConnection(
+            [
+                'driver' => $container->getParameter('database_driver'),
+                'host' => $container->getParameter('database_host'),
+                'port' => $container->getParameter('database_port'),
+                'user' => $container->getParameter('database_user'),
+                'password' => $container->getParameter('database_password'),
+                'charset' => 'UTF8',
+            ]
+        );
+        $name = $connection->getDatabasePlatform()->quoteSingleIdentifier($name);
+
+        $connection->getSchemaManager()->dropAndCreateDatabase($name);
+        $connection->close();
+
+        if ($this->getSetUpDbFile() !== null) {
+            $this->executeLargeSqlFile(static::getRootDir($container) . $this->getSetUpDbFile());
+        }
+    }
+
+    /**
+     * Deletes the database.
+     */
+    public static function tearDownAfterClass()
+    {
+        $container = static::createClient()->getContainer();
+        /** @var EntityManager $entityManager */
+        $entityManager = $container->get('doctrine')->getManager();
+
+        $connection = $entityManager->getConnection();
+        $name = $container->getParameter('database_name');
+        $name = $connection->getSchemaManager()->getDatabasePlatform()->quoteSingleIdentifier($name);
+
+        $connection->getSchemaManager()->dropDatabase($name);
+    }
 
     /**
      * Gets entity manager.
@@ -50,41 +100,13 @@ abstract class TestBase extends WebTestCase
     }
 
     /**
-     * Sets up required info before each test.
+     * Return full route to db file.
+     *
+     * @return string
      */
-    protected function setUp()
+    public function getSetUpDbFile()
     {
-        AnnotationRegistry::registerFile(
-            'vendor/doctrine/orm/lib/Doctrine/ORM/Mapping/Driver/DoctrineAnnotations.php'
-        );
-        /** @var EntityManager $entityManager */
-        $entityManager = $this->getEntityManager();
-        $connection = $entityManager->getConnection();
-        $params = $connection->getParams();
-        $name = $connection->getParams()['dbname'];
-        unset($params['dbname']);
-        $tmpConnection = DriverManager::getConnection($params);
-        $name = $tmpConnection->getDatabasePlatform()->quoteSingleIdentifier($name);
-        try {
-            $tmpConnection->getSchemaManager()->dropDatabase($name);
-        } catch (\Exception $ex) {
-            // Catching exception in case database not dropped.
-        }
-        $tmpConnection->getSchemaManager()->createDatabase($name);
-        $tmpConnection->close();
-    }
-
-    /**
-     * Deletes  all data after each test.
-     */
-    protected function tearDown()
-    {
-        /** @var EntityManager $entityManager */
-        $entityManager = $this->getEntityManager();
-        $connection = $entityManager->getConnection();
-        $name = $connection->getParams()['dbname'];
-        $name = $connection->getSchemaManager()->getDatabasePlatform()->quoteSingleIdentifier($name);
-        $connection->getSchemaManager()->dropDatabase($name);
+        return null;
     }
 
     /**
@@ -115,19 +137,6 @@ abstract class TestBase extends WebTestCase
     }
 
     /**
-     * Executes an SQL file.
-     *
-     * @param Connection $conn
-     * @param string     $file
-     */
-    protected function executeSqlFile(Connection $conn, $file)
-    {
-        $sql = file_get_contents($file);
-        $stmt = $conn->prepare($sql);
-        $stmt->execute();
-    }
-
-    /**
      * Compares two sets of records (suited for sync jobs data comparison).
      *
      * @param array $expectedRecords
@@ -153,5 +162,62 @@ abstract class TestBase extends WebTestCase
         }
 
         $this->assertEquals($expectedRecords, $actualRecords);
+    }
+
+    /**
+     * Executes an SQL file.
+     *
+     * @param Connection $conn
+     * @param string     $file
+     */
+    protected function executeSqlFile(Connection $conn, $file)
+    {
+        $sql = file_get_contents($file);
+        $stmt = $conn->prepare($sql);
+        $stmt->execute();
+    }
+
+    /**
+     * Executes large SQL file.
+     *
+     * @param string $filename
+     */
+    protected function executeLargeSqlFile($filename)
+    {
+        $container = static::createClient()->getContainer();
+        /** @var EntityManager $manager */
+        $manager = $container->get('doctrine')->getManager();
+        $connection = $manager->getConnection();
+        $tempLine = '';
+        $lines = file($filename);
+
+        foreach ($lines as $line) {
+            // Skip it if it's a comment.
+            if (substr($line, 0, 2) == '--' || $line == '') {
+                continue;
+            }
+
+            // Add this line to the current segment.
+            $tempLine .= $line;
+
+            // If it has a semicolon at the end, it's the end of the query.
+            if (substr(trim($line), -1, 1) == ';') {
+                $connection->exec($tempLine);
+                // Reset temp variable to empty.
+                $tempLine = '';
+            }
+        }
+    }
+
+    /**
+     * Return full path to kernel root dir.
+     *
+     * @param ContainerInterface $container
+     *
+     * @return string
+     */
+    protected static function getRootDir($container)
+    {
+        return $container->get('kernel')->getRootDir();
     }
 }
