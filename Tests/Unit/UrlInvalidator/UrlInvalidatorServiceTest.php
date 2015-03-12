4965bd07f1b7687a8ff92ff1fa35dd0c07635881
Lukas Mikelionis <lukas.mikelionis@ongr.io>
2015-03-12 18:09:52 +0200

    Tests updated

--- Tests/Unit/UrlInvalidator/UrlInvalidatorServiceTest.php
+++ Tests/Unit/UrlInvalidator/UrlInvalidatorServiceTest.php
@@ -16,6 +16,7 @@ use ONGR\ConnectionsBundle\UrlInvalidator\UrlInvalidatorService;
 use ONGR\ElasticsearchBundle\ORM\Manager;
 use ONGR\RouterBundle\Document\SeoAwareInterface;
 use ONGR\RouterBundle\Document\UrlObject;
+use ReflectionClass;
 use Symfony\Component\Routing\RouterInterface;
 
 /**
@@ -238,5 +239,86 @@ class UrlInvalidatorServiceTest extends \PHPUnit_Framework_TestCase
         );
 
         unlink($file);
+    }
+
+    /**
+     * Change visibility for protected methods.
+     *
+     * @param string $className
+     * @param string $method
+     * @param null   $property
+     *
+     * @return object
+     */
+    protected function changeMethodType($className, $method = null, $property = null)
+    {
+        $class = new ReflectionClass($className);
+
+        if ($method !== null) {
+            $method = $class->getMethod($method);
+            $method->setAccessible(true);
+        }
+
+        if ($property !== null) {
+            $property = $class->getProperty($property);
+            $property->setAccessible(true);
+        }
+
+        $router = $this->getMock('Symfony\Component\Routing\RouterInterface');
+        $instance = new $className(
+            $router,
+            $this->manager,
+            'http://ongr.dev',
+            100,
+            '',
+            '',
+            true
+        );
+
+        return [$method, $instance, $property];
+    }
+
+    /**
+     * Test methods that needs mock.
+     */
+    public function testInternals()
+    {
+        $className = 'ONGR\ConnectionsBundle\UrlInvalidator\UrlInvalidatorService';
+
+        // Test protected addWildcard.
+        $mockAddWildcard = $this->changeMethodType($className, 'addWildcard');
+        $testItems = [
+            ['/test/', '/test/*'],
+            ['/test', '/test'],
+        ];
+
+        foreach ($testItems as $item) {
+            $mockAddWildcard[0]->invokeArgs($mockAddWildcard[1], [ & $item[0]]);
+            $this->assertEquals($item[0], $item[1]);
+        }
+
+        // Test protected setInvalidateSeoUrls.
+        $mockSeoUrls = $this->changeMethodType($className, 'setInvalidateSeoUrls', 'invalidateSeoUrls');
+        $seoItem = 'item';
+        $mockSeoUrls[0]->invokeArgs($mockSeoUrls[1], [$seoItem]);
+
+        $this->assertEquals($mockSeoUrls[2]->getValue($mockSeoUrls[1]), $seoItem);
+
+        // Test addDocumentParameter.
+        $mockDocParam = $this->changeMethodType($className, 'addDocumentParameter', 'documentParamCache');
+        $field = 'field';
+        $value = 'value';
+
+        $mockDocParam[0]->invokeArgs($mockDocParam[1], [$field, $value]);
+
+        $md5Key = md5($value . $field);
+        $array = $mockDocParam[2]->getValue($mockDocParam[1]);
+
+        $this->assertEquals($array[$md5Key], [$field, $value]);
+
+        // Test resetCache.
+        $mockResetCache = $this->changeMethodType($className, 'resetCache', 'invalidateSeoUrls');
+        $mockResetCache[2]->setValue($mockResetCache[1], ['a', 'b']);
+        $mockResetCache[0]->invokeArgs($mockResetCache[1], []);
     }
 }
